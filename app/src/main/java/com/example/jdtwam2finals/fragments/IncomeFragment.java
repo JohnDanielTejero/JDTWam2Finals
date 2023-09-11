5def5d6c9011c2555740179cf080b65c6aeb9bc2
John Daniel Tejero <96475894+JohnDanielTejero@users.noreply.github.com>
2023-09-11 10:11:19 +0800

    Update IncomeFragment.java
    added insert option

--- app/src/main/java/com/example/jdtwam2finals/fragments/IncomeFragment.java
+++ app/src/main/java/com/example/jdtwam2finals/fragments/IncomeFragment.java
@@ -1,15 +1,36 @@
 package com.example.jdtwam2finals.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.jdtwam2finals.R;
+import com.example.jdtwam2finals.dao.DbCon;
+import com.example.jdtwam2finals.dao.IncomeTable;
+import com.example.jdtwam2finals.dao.QueryBuilder;
+import com.example.jdtwam2finals.dao.TransactionTable;
 import com.example.jdtwam2finals.databinding.FragmentIncomeBinding;
+import com.example.jdtwam2finals.dto.Income;
+import com.example.jdtwam2finals.dto.Transaction;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +40,11 @@ import com.example.jdtwam2finals.databinding.FragmentIncomeBinding;
 public class IncomeFragment extends Fragment {
 
     private FragmentIncomeBinding b;
+    private TextInputEditText amount, note;
+    private Button submit;
+    private Context context;
+    private DbCon dbCon;
+    private SharedPreferences sp;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -57,6 +83,7 @@ public class IncomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        context = requireActivity();
     }
 
     @Override
@@ -65,5 +92,60 @@ public class IncomeFragment extends Fragment {
         // Inflate the layout for this fragment
         b = FragmentIncomeBinding.inflate(inflater, container, false);
         return b.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        sp = context.getSharedPreferences("login", Context.MODE_PRIVATE);
+        dbCon = DbCon.getInstance(context);
+        submit = b.incomeSubmit;
+        amount = b.incomeAmount;
+        note = b.incomeNote;
+
+        submit.setOnClickListener(v -> {
+
+            boolean amountIsSubmittable = true;
+            boolean noteIsSubmittable = true;
+
+            if (amount.getText().toString().isEmpty()){
+                amount.setError("Field is required!");
+                amountIsSubmittable = false;
+            }
+
+            if(note.getText().toString().isEmpty()){
+                note.setError("Field is required!");
+                noteIsSubmittable = false;
+            }
+
+            if (amountIsSubmittable && noteIsSubmittable){
+
+                QueryBuilder<Transaction> transactionBuilder = new TransactionTable(dbCon.getWritableDatabase());
+                int userId = sp.getInt("user", -1);
+                long transactionId = transactionBuilder.insert(new Transaction("Income", new Date(), (int) userId));
+                if (transactionId != -1){
+                    Log.d("insertTransaction", "Transaction successfully inserted: " + transactionId);
+                    QueryBuilder<Income> incomeQueryBuilder = new IncomeTable(dbCon.getWritableDatabase());
+
+                    long incomeId = incomeQueryBuilder.insert(new Income(
+                            Double.parseDouble(amount.getText().toString()),
+                            note.getText().toString(),
+                            (int) transactionId
+                            ));
+                    if (incomeId != -1){
+                        Log.d("insertTransaction", "Income inserted: " + incomeId);
+                        Toast.makeText(context, "Income inserted", Toast.LENGTH_SHORT).show();
+                        amount.setText("");
+                        note.setText("");
+                    }else{
+                        Log.d("insertTransaction", "Income did not insert, deleting transaction: " + transactionId);
+                    }
+                }
+
+            }else{
+                Toast.makeText(context, "Field is required!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
