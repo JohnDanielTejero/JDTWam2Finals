c079e1807a788942740f5f73678d6f63329e3e3a
John Daniel Tejero <96475894+JohnDanielTejero@users.noreply.github.com>
2023-09-11 10:11:00 +0800

    Update ExpenseFragment.java
    added insert operation

--- app/src/main/java/com/example/jdtwam2finals/fragments/ExpenseFragment.java
+++ app/src/main/java/com/example/jdtwam2finals/fragments/ExpenseFragment.java
@@ -1,15 +1,33 @@
 package com.example.jdtwam2finals.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.jdtwam2finals.R;
+import com.example.jdtwam2finals.dao.DbCon;
+import com.example.jdtwam2finals.dao.ExpenseTable;
+import com.example.jdtwam2finals.dao.IncomeTable;
+import com.example.jdtwam2finals.dao.QueryBuilder;
+import com.example.jdtwam2finals.dao.TransactionTable;
 import com.example.jdtwam2finals.databinding.FragmentExpenseBinding;
+import com.example.jdtwam2finals.dto.Expense;
+import com.example.jdtwam2finals.dto.Income;
+import com.example.jdtwam2finals.dto.Transaction;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +37,11 @@ import com.example.jdtwam2finals.databinding.FragmentExpenseBinding;
 public class ExpenseFragment extends Fragment {
 
     private FragmentExpenseBinding b;
+    private TextInputEditText amount, category, note;
+    private Button submit;
+    private Context context;
+    private DbCon dbCon;
+    private SharedPreferences sp;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -57,6 +80,7 @@ public class ExpenseFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        context = requireActivity();
     }
 
     @Override
@@ -65,5 +89,69 @@ public class ExpenseFragment extends Fragment {
         b = FragmentExpenseBinding.inflate(inflater, container, false);
         // Inflate the layout for this fragment
         return b.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        sp = context.getSharedPreferences("login", Context.MODE_PRIVATE);
+        dbCon = DbCon.getInstance(context);
+        amount = b.expenseAmount;
+        category = b.expenseCategory;
+        note = b.expenseNote;
+        submit = b.expenseSubmit;
+
+        submit.setOnClickListener(v -> {
+
+            boolean amountIsSubmittable = true;
+            boolean noteIsSubmittable = true;
+            boolean categoryIsSubmittable = true;
+
+            if (amount.getText().toString().isEmpty()){
+                amount.setError("Field is required!");
+                amountIsSubmittable = false;
+            }
+
+            if(note.getText().toString().isEmpty()){
+                note.setError("Field is required!");
+                noteIsSubmittable = false;
+            }
+
+            if (category.getText().toString().isEmpty()){
+                category.setError("Field is required!");
+                categoryIsSubmittable = false;
+            }
+
+            if (amountIsSubmittable && noteIsSubmittable && categoryIsSubmittable){
+
+                QueryBuilder<Transaction> transactionBuilder = new TransactionTable(dbCon.getWritableDatabase());
+                int userId = sp.getInt("user", -1);
+                long transactionId = transactionBuilder.insert(new Transaction("Income", new Date(), (int) userId));
+                if (transactionId != -1){
+                    Log.d("insertTransaction", "Transaction successfully inserted: " + transactionId);
+                    QueryBuilder<Expense> expenseQueryBuilder = new ExpenseTable(dbCon.getWritableDatabase());
+
+                    long expenseId = expenseQueryBuilder.insert(new Expense(
+                            Double.parseDouble(amount.getText().toString()),
+                            category.getText().toString(),
+                            note.getText().toString(),
+                            (int) transactionId
+                    ));
+                    if (expenseId != -1){
+                        Log.d("insertTransaction", "Expense inserted: " + expenseId);
+                        Toast.makeText(context, "Expense inserted", Toast.LENGTH_SHORT).show();
+                        amount.setText("");
+                        note.setText("");
+                        category.setText("");
+                    }else{
+                        Log.d("insertTransaction", "Expense did not insert, deleting transaction: " + transactionId);
+                    }
+                }
+
+            }else{
+                Toast.makeText(context, "Field is required!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
